ce2789fb10c189aca0f3632d6ec7308dfdc88bed
xsstnv <xsstnv@gmail.com>
2021-04-02 20:07:50 +0300

    cleared some junk

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from './containers/layout/Layout';
 import { BrowserRouter as Router } from "react-router-dom";
-import './App.css';
 
 function App() {
   return (
