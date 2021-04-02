dd1a964f181507595d453f352d4fc61ab67399dc
xsstnv <xsstnv@gmail.com>
2021-04-02 20:23:33 +0300

    set to custom domain

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Layout from './containers/layout/Layout';
 import { BrowserRouter as Router } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Layout />
-      </Router>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+  <Router>
+    <Layout />
+  </Router>
+</div>
+);
 
 export default App;
