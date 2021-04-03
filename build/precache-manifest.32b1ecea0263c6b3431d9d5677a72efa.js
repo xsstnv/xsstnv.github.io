9edf9e3f50581783d876c5a4d6582fc4d9773cc1
xsstnv <xsstnv@gmail.com>
2021-04-03 11:11:09 +0300

    creating dev branch to keep development branch and deploying branch separate

---
+++ build/precache-manifest.32b1ecea0263c6b3431d9d5677a72efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466535d0e01d7b00706a87c82324093c",
+    "url": "/stnv.dev/index.html"
+  },
+  {
+    "revision": "f22a2d3d4712853b14c6",
+    "url": "/stnv.dev/static/css/main.91104c5e.chunk.css"
+  },
+  {
+    "revision": "13a29b7fb829441fd561",
+    "url": "/stnv.dev/static/js/2.c5bc856d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/stnv.dev/static/js/2.c5bc856d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22a2d3d4712853b14c6",
+    "url": "/stnv.dev/static/js/main.a153dcd1.chunk.js"
+  },
+  {
+    "revision": "3054b0935c20cbc9ff1e",
+    "url": "/stnv.dev/static/js/runtime-main.281c2754.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79b342ec8d44bdd63575d14e93c42f11.js"
+  "/stnv.dev/precache-manifest.32b1ecea0263c6b3431d9d5677a72efa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stnv.dev/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
