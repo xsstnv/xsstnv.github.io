0f5a804b256bfcb94da19150564ab9aac52a0b23
xsstnv <xsstnv@gmail.com>
2021-04-05 18:08:28 +0300

    fixed routes, added some packages, removed some useless components

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import classes from './Content.module.css';
-import { routes } from '../../routes';
+import Routes from '../Routes/Routes';
 
 const Pages = () => (
     <div className={classes.Content}>
         <main className={classes.Main}>
-            {routes}
+            <Routes />
         </main>
     </div>
 );
---
+++ src/components/Routes/Routes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { getMapping } from '../../redux/actions/mapping';
+import MarkdownReader from '../MarkdownReader/MarkdownReader';
+
+const Routes = props => {
+
+    React.useEffect(() => {
+        props.getMapping();
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+        <Switch>
+            { props.mapping && props.mapping.mapping && props.mapping.mapping.length && props.mapping.mapping.map((item, key) => (
+                <Route key={key} exact path={item.url}>
+                    <MarkdownReader path={process.env.REACT_APP_PAGES + item.path} key={key} />
+                </Route>
+            ))}
+        </Switch>
+    );
+}
+
+const mapStateToProps = state => ({
+    mapping: state.mapping,
+});
+
+const mapDispatchToProps = dispatch => ({
+    getMapping: () => dispatch(getMapping()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Routes);
